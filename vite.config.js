4de490ac39ec133250fcc3ec1498408d7935e2fa
Ragav <sriragavandhandapani@gmail.com>
2026-02-09 22:46:12 +0530

    Vite config issue

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,11 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: "build",
+    outDir: 'dist',
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   base: '/',
-  build:{
-    outDir: 'dist',
-  }
   server: {
     port: "4028",
     host: "0.0.0.0",
